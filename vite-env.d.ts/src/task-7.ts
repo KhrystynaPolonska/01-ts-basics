f732fbc9710528ea40fa673e353d59c0c52e9cfb
kristy <krisposol7@gmail.com>
2025-09-26 11:07:14 +0200

    add1

---
+++ vite-env.d.ts/src/task-7.ts
@@ -0,0 +1,11 @@
+// task-7.ts
+
+function getMessage(): Promise<string> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello from TS");
+    }, 1000);
+  });
+}
+
+getMessage().then((result) => console.log(result));
