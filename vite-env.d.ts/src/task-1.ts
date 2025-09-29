92c6e238b13faba0bc23eed32bacd184e3ac364c
kristy <krisposol7@gmail.com>
2025-09-29 22:47:02 +0200

    4

--- vite-env.d.ts/src/task-1.ts
+++
@@ -1,9 +0,0 @@
-// task-1.ts
-
-const name: string = "Alice";
-const age: number = 30;
-const isOnline: boolean = true;
-
-console.log(`Name: ${name}`);
-console.log(`Age: ${age}`);
-console.log(`Online: ${isOnline}`);
